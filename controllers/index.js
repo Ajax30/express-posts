efe2527cb9cec38daeb765c9d020bcc5c567d0c4
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2021-10-18 23:17:33 +0300

    Use base_url; get posts by author (and dysplay author name)

--- controllers/index.js
+++ controllers/index.js
@@ -5,8 +5,8 @@ var helpers = require('../utils/helpers');
 exports.getHomepageData = async (req, res, next) => {
 
   let [userData, postData] =  await Promise.all([
-    axios.get('https://jsonplaceholder.typicode.com/users'),
-    axios.get('https://jsonplaceholder.typicode.com/posts') 
+    axios.get(`${base_url}/users`),
+    axios.get(`${base_url}/posts`) 
   ]);
 
   const users = userData.data;
@@ -16,8 +16,37 @@ exports.getHomepageData = async (req, res, next) => {
   {
     layout: 'layout',
     title: 'Express',
+    pageTitle: 'All posts',
     users: users,
     posts: posts,
+
+   }
+  );
+};
+
+/* GET posts by user */
+exports.getPostsByUser = async (req, res, next) => {
+
+  let uid = req.params.uid;
+
+  let [userData, currentUserData, postData] =  await Promise.all([
+    axios.get(`${base_url}/users`),
+    axios.get(`${base_url}/users/${uid}`),
+    axios.get(`${base_url}/posts?userId=${uid}`) 
+  ]);
+
+  const users = userData.data;
+  const currentUser = currentUserData.data;
+  const posts = postData.data;
+
+  res.render('index',
+  {
+    layout: 'layout',
+    title: 'Express',
+    users: users,
+    posts: posts,
+    currentUser: currentUser,
+    pageTitle: `All posts by ${currentUser.name}`,
 
    }
   );
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,7 @@ const router = express.Router();
 // Get Posts
 router.get('/', indexController.getHomepageData);
 
+// Get Posts By User
+router.get('/:uid', indexController.getPostsByUser);
+
 module.exports = router;
