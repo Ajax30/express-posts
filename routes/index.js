399f6f204dfa42472333cecad8d4c7df883207e1
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2021-10-19 09:24:44 +0300

    Redirect from bad routes

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,13 @@ router.get('/', indexController.getHomepageData);
 // Get Posts By User
 router.get('/users/:uid/posts/', indexController.getPostsByUser);
 
+// Redirect from bad routes
+router.get('/users/', function(req, res) {
+    res.redirect('/');
+});
+
+router.get('/users/:uid/', function(req, res) {
+    res.redirect('/');
+});
+
 module.exports = router;
