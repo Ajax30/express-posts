dd530196d57c1f538adaf41a8989655e431ecb94
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2021-10-21 11:13:20 +0300

    add trycatch

--- controllers/index.js
+++ controllers/index.js
@@ -3,31 +3,33 @@ var helpers = require('../utils/helpers');
 
 /* GET home page data */
 exports.getHomepageData = async (req, res, next) => {
-
-  let [userData, postData] =  await Promise.all([
-    axios.get(`${base_url}/users`),
-    axios.get(`${base_url}/posts`) 
-  ]);
-
-  const users = userData.data;
-  const posts = postData.data;
-
-  res.render('index',
-  {
-    layout: 'layout',
-    pageTitle: 'All posts',
-    users: users,
-    currentUser: null,
-    posts: posts,
-
-   }
-  );
+  try {
+    let [userData, postData] =  await Promise.all([
+      axios.get(`${base_url}/users`),
+      axios.get(`${base_url}/posts`) 
+    ]);
+  
+    const users = userData.data;
+    const posts = postData.data;
+  
+    res.render('index',
+    {
+      layout: 'layout',
+      pageTitle: 'All posts',
+      users: users,
+      currentUser: null,
+      posts: posts,
+  
+     });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 /* GET posts by user */
 exports.getPostsByUser = async (req, res, next) => {
-
-  let uid = req.params.uid;
+  try {
+    let uid = req.params.uid;
 
   let [userData, currentUserData, postData] =  await Promise.all([
     axios.get(`${base_url}/users`),
@@ -47,4 +49,7 @@ exports.getPostsByUser = async (req, res, next) => {
     currentUser: currentUser,
     pageTitle: `Posts by ${currentUser.name}`,
   });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
